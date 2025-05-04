45b8993aa8903dff9b72c01c853f8f1da32f2e2b
Henry Nunez <henryda2004@gmail.com>
2025-05-04 14:27:33 -0600

    Se organiza el main para hacer pruebas mas simples de los algortimos de calendarización

--- main.c
+++ main.c
@@ -7,7 +7,9 @@ int main(void)
         {66, 1, 2, 10},
         {99, 2, 5,  5},
         {200, 3, 5,  5},
-        {100, 4, 5,  5}
+        {99, 4, 5,  5},
+        {200, 5, 5,  5},
+        {100, 6, 5,  5}
 
     };
     int n = sizeof(carga)/sizeof(carga[0]);
@@ -17,7 +19,7 @@ int main(void)
     // run_test(SCHED_CE_RR);        // Round Robin
     // run_test(SCHED_CE_REALTIME);  // Tiempo Real (según deadline)
 
-    ce_run_plan(carga, n, SCHED_CE_PRIORITY, /*quantum*/5);
+    ce_run_plan(carga, n, SCHED_CE_RR, /*quantum*/5);
     /* Para probar otro modo:                          *
      * ce_run_plan(carga, n, SCHED_CE_PRIORITY, 0);    */
 
