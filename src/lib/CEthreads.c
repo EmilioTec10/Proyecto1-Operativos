fc81a9fecfd129c79f1fcc306611c0c6f077c750
kunZhen <kzheng@estudiantec.cr>
2025-05-15 02:09:21 -0600

    [UPDATE] CEthreads

--- src/lib/CEthreads.c
+++ src/lib/CEthreads.c
@@ -1,118 +1,109 @@
-#define _GNU_SOURCE
-#include <sched.h>
-#include <sys/types.h>
+#include "CEthreads.h"
+
 #include <linux/sched.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <errno.h>
-#include <stdatomic.h>
-#include <linux/futex.h>
-#include <sys/syscall.h>
-#include <stdint.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>  // For waitpid
+#include <unistd.h>
 
-#define STACK_SIZE (1024 * 1024)
+// Thread-local variable to store the current thread's argument
+static __thread thread_arg_t *current_thread_arg = NULL;
 
-typedef struct {
-    pid_t tid;
-} CEthread_t;
+// Wrapper function that runs the thread routine
+int thread_start(void *arg) {
+  thread_arg_t *thread_arg = (thread_arg_t *)arg;
+  current_thread_arg = thread_arg;
 
-typedef struct {
-    atomic_int value;
-} CEmutex_t;
+  // Execute the thread function
+  void *retval = thread_arg->start_routine(thread_arg->arg);
+  thread_arg->retval = retval;
 
-typedef int (*CEthread_start_routine)(void *);
-
-struct thread_args {
-    CEthread_start_routine func;
-    void *arg;
-};
-
-int thread_entry(void *void_args) {
-    struct thread_args *args = (struct thread_args *)void_args;
-    int result = args->func(args->arg);
-    free(args);
-    return result;
+  // Properly terminate the thread
+  _exit(0);
 }
 
-int CEthread_create(CEthread_t *thread, void *(*start_routine)(void *), void *arg) {
-    void *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
-                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
-    if (stack == MAP_FAILED) {
-        perror("mmap");
-        return -1;
-    }
+int CEthread_create(CEthread_t *thread, void *(*start_routine)(void *),
+                    void *arg) {
+  // Allocate memory for the stack
+  thread->stack = malloc(STACK_SIZE);
+  if (thread->stack == NULL) {
+    return -1;  // Error allocating the stack
+  }
 
-    void *stack_top = (char *)stack + STACK_SIZE;
+  // Allocate memory for the thread arguments
+  thread->thread_arg = malloc(sizeof(thread_arg_t));
+  if (thread->thread_arg == NULL) {
+    free(thread->stack);
+    return -1;  // Error allocating thread_arg
+  }
+  thread->thread_arg->start_routine = start_routine;
+  thread->thread_arg->arg = arg;
+  thread->thread_arg->retval = NULL;
 
-    struct thread_args *args = malloc(sizeof(struct thread_args));
-    if (!args) {
-        munmap(stack, STACK_SIZE);
-        return -1;
-    }
-    args->func = (CEthread_start_routine)start_routine;
-    args->arg = arg;
+  // Calculate the top of the stack (it grows downward)
+  void *stack_top = (char *)thread->stack + STACK_SIZE;
 
-    int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | SIGCHLD;
+  // Create the thread using clone
+  thread->tid =
+      clone(thread_start, stack_top,
+            SIGCHLD | CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND,
+            thread->thread_arg);
+  if (thread->tid == -1) {
+    free(thread->stack);
+    free(thread->thread_arg);
+    return -1;  // Error creating the thread
+  }
 
-    pid_t tid = clone(thread_entry, stack_top, flags, args);
-    if (tid == -1) {
-        perror("clone");
-        free(args);
-        munmap(stack, STACK_SIZE);
-        return -1;
-    }
-
-    thread->tid = tid;
-    return 0;
+  return 0;  // Success
 }
 
-int CEthread_join(CEthread_t thread) {
-    int status;
-    if (waitpid(thread.tid, &status, 0) == -1) {
-        if (errno == ECHILD) return 0;
-        perror("waitpid");
-        return -1;
-    }
+int CEthread_join(CEthread_t thread, void **retval) {
+  // Wait for the thread to finish
+  pid_t pid = waitpid(thread.tid, NULL, __WALL);
+  if (pid == -1) return -1;  // Error waiting for the thread
 
-    if (WIFEXITED(status)) {
-        return WEXITSTATUS(status);
-    }
+  // Retrieve the return value
+  if (retval != NULL) *retval = thread.thread_arg->retval;
 
-    return -1;
+  // Release the allocated resources
+  free(thread.stack);
+  free(thread.thread_arg);
+
+  return 0;  // Success
 }
 
+void CEthread_end(void *retval) {
+  if (current_thread_arg != NULL) current_thread_arg->retval = retval;
+
+  _exit(0);  // Terminate the current thread
+}
+
+/**********************
+******** Mutex ********
+***********************/
 int CEmutex_init(CEmutex_t *mutex) {
-    atomic_store(&mutex->value, 0);
-    return 0;
+  atomic_flag_clear(&mutex->flag);  // De stdatomic.h
+  return 0;
 }
 
 int CEmutex_destroy(CEmutex_t *mutex) {
-    if (atomic_load(&mutex->value) != 0) {
-        return -1;
-    }
-    atomic_store(&mutex->value, -1);
-    return 0;
+  if (atomic_flag_test_and_set(&mutex->flag)) {
+    printf("Error: intento de destruir un mutex aún bloqueado.\n");
+    return 1;
+  }
+  return 0;
 }
 
 int CEmutex_lock(CEmutex_t *mutex) {
-    int expected;
-
-    while (1) {
-        expected = 0;
-        if (atomic_compare_exchange_strong(&mutex->value, &expected, 1)) {
-            return 0;
-        }
-
-        syscall(SYS_futex, &mutex->value, FUTEX_WAIT, 1, NULL, NULL, 0);
-    }
+  while (atomic_flag_test_and_set(&mutex->flag)) {
+    // Spinlock - Espera activa
+  }
+  return 0;
 }
 
 int CEmutex_unlock(CEmutex_t *mutex) {
-    atomic_store(&mutex->value, 0);
-    syscall(SYS_futex, &mutex->value, FUTEX_WAKE, 1, NULL, NULL, 0);
-    return 0;
+  atomic_flag_clear(&mutex->flag);
+  return 0;
 }
--- src/lib/CEthreads.h
+++ src/lib/CEthreads.h
@@ -1,23 +1,40 @@
-#ifndef CETHREADS_H
-#define CETHREADS_H
+#ifndef CETHREAD_H
+#define CETHREAD_H
 
+#include <stdatomic.h>
+#include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
+
+#ifndef STACK_SIZE
+#define STACK_SIZE (1024 * 1024)  // 1MiB
+#endif // STACK_SIZE
 
 typedef struct {
-    pid_t tid;
+  void *(*start_routine)(void *);
+  void *arg;
+  void *retval;
+} thread_arg_t;
+
+typedef struct {
+  pid_t tid;                 // id thread
+  void *stack;               // pointer to stack
+  thread_arg_t *thread_arg;  // argument
 } CEthread_t;
 
 typedef struct {
-    _Atomic int value;
+  atomic_flag flag;
 } CEmutex_t;
 
-// Firma de funciones disponibles
-int CEthread_create(CEthread_t *thread, void *(*start_routine)(void *), void *arg);
-int CEthread_join(CEthread_t thread);
+// Context from Function src/lib/cethread.c
+int CEthread_create(CEthread_t *thread, void *(*start_routine)(void *),
+                    void *arg);
+int CEthread_join(CEthread_t thread, void **retval);
+void CEthread_end(void *retval);
 
 int CEmutex_init(CEmutex_t *mutex);
 int CEmutex_destroy(CEmutex_t *mutex);
-int cccccc(CEmutex_t *mutex);
+int CEmutex_lock(CEmutex_t *mutex);
 int CEmutex_unlock(CEmutex_t *mutex);
 
-#endif
+#endif  // CETHREAD_H
